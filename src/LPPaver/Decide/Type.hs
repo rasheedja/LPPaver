f2678999873f657dbfcfb87a0da952cd584553eb
Junaid Rasheed <jrasheed178@gmail.com>
2022-12-06 19:33:57 +0000

    Update pavings types
    + Fixup into relevant commit

--- src/LPPaver/Decide/Type.hs
+++ src/LPPaver/Decide/Type.hs
@@ -15,34 +15,20 @@ import LPPaver.Constraint.Type
 import PropaFP.Expression (ESafe)
 import AERN2.BoxFun.Type (BoxFun)
 
--- |Possible results of evaluating a box using either interval methods or by linearisations
-data BoxEvalResult = UnsatEval | UnsatLin | IndetEval | IndetLin | SatEval | SatLin | NoEval | Unknown
-  deriving (P.Eq, Show)
-
--- |Whether a box was split, pruned, the initial (starting) box, or was unchanged
-data BoxPavingType = Prune | Split | Start | None
-  deriving (P.Eq, Show)
-
--- |Stores information on how our paving algorithm leads to boxes of this type.
--- Usage: BoxPaving {yourBox} {resultOfEvaluatingTheBox} {howWeGotToThisBoxFromThePreviousBox}
-data BoxPaving boxType = BoxPaving boxType BoxEvalResult BoxPavingType deriving (P.Eq, Show)
-
--- |A list of 'BoxPaving'
-type BoxPavings boxType = [BoxPaving boxType]
-
 -- |The result of evaluating a box as well as a list of pavings that leads to said box
-data BoxResult boxType = SatBox boxType (BoxPavings boxType) | UnsatBox (BoxPavings boxType) | IndeterminateBox boxType (BoxPavings boxType)
+data BoxStep box = Initial box | EvalTrue box | EvalFalse box | GaveUp box | Split box box box | Contract box box | ContractEmpty box | FoundModel box box 
   deriving (P.Eq, Show)
 
--- |The 'BoxResult' of a conjunction within a DNF.
-type DNFConjunctionResult boxType = BoxResult boxType
-
--- |The result of evaluating a DNF over a certain box as well as a list of pavings that leads to said result.
-data DNFResult boxType = SatDNF boxType (BoxPavings boxType) | UnsatDNF [BoxPavings boxType] | IndeterminateDNF boxType (BoxPavings boxType)
+-- |The result of a box against some conjunction as well as a list of steps that leads to said result.
+data DNFConjunctionResult box = SatBox box [BoxStep box] | UnsatBox [BoxStep box] | IndeterminateBox box [BoxStep box]
   deriving (P.Eq, Show)
 
--- |A type used to hold an 'ESafe' expression with it's equivalent 'BoxFun'.
-type ESafeWithBoxFun = (ESafe, BoxFun)
+-- |The result of a box against some DNF.
+-- If sat, give a list of pavings that leads to the satisfiable box.
+-- If indeterminate, give a list of pavings that leads to the indeterminate box for an arbitrary conjunction in the DNF.
+-- If unsat, give a list of list of pavings. Each inner list show the steps for each conjunction that lead to the unsat result.
+data DNFResult box = SatDNF box [BoxStep box] | UnsatDNF [[BoxStep box]] | IndeterminateDNF box [BoxStep box]
+  deriving (P.Eq, Show)
 
 -- |Information required for an efficient best-first queue algorithm
-data DNFConjunctionQueueInfo = QueueInfo {expressionsWithFunctions :: [ESafeWithBoxFun], wasLeftCornerLinearisedLast :: Bool, wasBoxPruned :: Bool}
+data DNFConjunctionQueueInfo = QueueInfo {expressionsWithFunctions :: [(ESafe, BoxFun)], wasLeftCornerLinearisedLast :: Bool}
