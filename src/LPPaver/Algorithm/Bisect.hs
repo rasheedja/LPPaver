98d6465f5737cbb59625b623ee5723c29099eb43
mikkonecny <mikkonecny@gmail.com>
2023-10-18 11:00:55 +0100

    configurable bisection strategy on aspect ratio

--- src/LPPaver/Algorithm/Bisect.hs
+++ src/LPPaver/Algorithm/Bisect.hs
@@ -11,14 +11,17 @@ module LPPaver.Algorithm.Bisect where
 
 import MixedTypesNumPrelude
 import qualified Prelude as P
-import Data.List (nub, sortBy, partition)
+import Data.List (nub, sortBy, partition, sortOn)
 import PropaFP.Parsers.Smt (findVariablesInExpressions)
 import qualified Numeric.CollectErrors as CN
 import qualified PropaFP.Expression as E
 import PropaFP.VarMap
 import qualified AERN2.Linear.Vector.Type as V
-import AERN2.MP (MPBall, IsBall (centre))
+import AERN2.MP (MPBall, IsBall (centre, radius))
 import AERN2.BoxFun.Box (getEndpoints, width)
+import AERN2.Util.Util (upperBound)
+import AERN2.MP.Float (infinity)
+import AERN2.MP.Dyadic (dyadic)
 
 -- import Debug.Trace (trace)
 trace a b = b
@@ -45,41 +48,71 @@ shouldBisectWithCoeffs coeffs typedVarMap filteredCornerRangesWithDerivatives = 
 {-|
   Splitting strategy for 2D problems given by a bunch of numeric coefficients.
 
-  The coefficients are for a linear form aligned to the following variables:
-  - the sum of widths
-  - width of variable 1
-  - width of variable 2
-  - fn 1 value in corner 1
-  - fn 1 centrepoint of difference in variable 1
-  - fn 1 centrepoint of difference in variable 2
-  - fn 2 value in corner 1
-  - fn 2 centrepoint of difference in variable 1
-  - fn 2 centrepoint of difference in variable 2
+  The strategy sets a desired 2D box aspect ratio based on the inputs:
+  - fn 1 slope in variable 1
+  - fn 1 slope in variable 2
+  - fn 1 direction uncertainty in variable 1
+  - fn 1 direction uncertainty in variable 2
+
+  Fn 1 is the function which seems to be nearest 0 with some certainty.
+
+  The given coefficients are used within a linear form over the above inputs.  The first coeff is the constant.
 -}
 bisectTypedVarMapWithCoeffs :: [Double] -> (TypedVarMap -> [(CN MPBall, CN MPBall, V.Vector (CN MPBall))] -> (TypedVarMap,TypedVarMap))
 bisectTypedVarMapWithCoeffs linCoeffs typedVarMap filteredCornerRangesWithDerivatives = bisectTypedVar typedVarMap selectedVar
   where
   selectedVar 
-    | fnValue < 0 = var1
+    | targetAspectRatio * w1 > w2 = var1 -- w1 is too large, split var1
     | otherwise = var2
-  fnValue = sum $ zipWith (*) linCoeffs $ [sum vmWidths] ++ vmWidths ++ fnCharacteristics2
 
-  ([var1, var2], vmWidths) = unzip $ map getVarWidth typedVarMap
+  targetAspectRatio = getAspectRatio fnValue
+
+  fnValue = sum $ zipWith (*) linCoeffs smallestFnCharacteristics
+
+  ([var1, var2], vmWidths@[w1, w2]) = unzip $ map getVarWidth typedVarMap
   getVarWidth (TypedVar (var, (l,r)) _) = (var, (double r)-l)
 
-  fnCharacteristics2 = flip trace fnCharacteristics $
-      "vmWidths = " ++ show vmWidths 
-      ++ "; fnCharacteristics = " ++ show fnCharacteristics
-      ++ "; filteredCornerRangesWithDerivatives = " ++ show filteredCornerRangesWithDerivatives
-  fnCharacteristics = concat $ map getFnValues filteredCornerRangesWithDerivatives
-  getFnValues (valCNMP, _, derivMPvector) = [valD] ++ (zipWith (*) derivs vmWidths)
+  -- fnCharacteristics2 = flip trace fnCharacteristics $
+  --     "vmWidths = " ++ show vmWidths 
+  --     ++ "; fnCharacteristics = " ++ show fnCharacteristics
+  --     ++ "; filteredCornerRangesWithDerivatives = " ++ show filteredCornerRangesWithDerivatives
+
+  smallestFnCharacteristics
+    | null fnCharacteristicsSorted = []
+    | otherwise = head fnCharacteristicsSorted
+  fnCharacteristicsSorted = map snd $ sortOn fst fnCharacteristics
+  fnCharacteristics = map getFnValues filteredCornerRangesWithDerivatives
+  getFnValues (val1CNMP, val2CNMP, derivMPvector) = (valU, [double 1] ++ derivsD ++ derivsUncertaintyD)
     where
-    valD = toDouble valCNMP
-    derivs = map toDouble $ V.toList derivMPvector
+    -- how close is the function is to 0? (assuming it is above 0)
+    valU = min val1U val2U
+    val1U = centreD (double infinity) (upperBound val1CNMP)
+    val2U = centreD (double infinity) (upperBound val2CNMP)
 
-toDouble :: CN MPBall -> Double
-toDouble cnmp = case CN.toEither cnmp of
+    derivsCNMP = V.toList derivMPvector
+    derivsD = map (centreD (double 0)) derivsCNMP
+    derivsUncertaintyD = map radiusD derivsCNMP
+
+maxAspectRatio = double 10
+
+getAspectRatio :: Double -> Double
+getAspectRatio t
+  | t < 0 = 1/(positiveRatio (- t))
+  | otherwise = positiveRatio t
+  where
+  positiveRatio :: Double -> Double
+  positiveRatio t = (maxAspectRatio - 1)*((logistic (t/10))*2 - 1) + 1
+
+logistic :: Double -> Double
+logistic x = 1 / (1 + (exp (- x)))
+
+centreD :: Double -> CN MPBall -> Double
+centreD badD cnmp = case CN.toEither cnmp of
     Right mp -> double $ AERN2.MP.centre mp
-    _ -> double 0
+    _ -> badD
 
+radiusD :: CN MPBall -> Double
+radiusD cnmp = case CN.toEither cnmp of
+  Right mp -> double $ dyadic $ radius mp
+  _ -> double infinity
 
